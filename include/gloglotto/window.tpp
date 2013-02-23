e137e868fcbb0df71b7331fe9e7fcc288b2632e4
meh <meh@schizofreni.co>
2013-02-23 22:57:11 +0100

    window: move GLUT related stuff out of thin

---
+++ include/gloglotto/window.tpp
@@ -0,0 +1,79 @@
+/** Copyleft (ɔ) meh. - http://meh.schizofreni.co
+ *
+ * This file is part of gloglotto - https://github.com/meh/gloglotto
+ *
+ * gloglotto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gloglotto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gloglotto. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gloglotto/utility>
+#include <gloglotto/window>
+
+namespace gloglotto
+{
+	namespace window
+	{
+		namespace callbacks
+		{
+			extern std::map<int, void*> closures;
+		}
+
+		template<int Callback, typename Function>
+		void
+		on (Function function) throw (std::invalid_argument, std::bad_typeid)
+		{
+			switch (Callback) {
+				case event::resize: signature_assert<void(int, int)>(function);
+					glutReshapeFunc(callbacks::resize);
+					break;
+
+				case event::render: signature_assert<void(void)>(function);
+					glutDisplayFunc(callbacks::render);
+					break;
+
+				case event::idle: signature_assert<void(void)>(function);
+					glutIdleFunc(callbacks::idle);
+					break;
+
+				case event::visible: signature_assert<void(bool)>(function);
+					glutVisibilityFunc(callbacks::visible);
+					break;
+
+				case event::keyboard: signature_assert<void(key, int, int)>(function);
+					glutKeyboardFunc(callbacks::keyboard);
+					glutKeyboardUpFunc(callbacks::keyboard_up);
+					glutSpecialFunc(callbacks::special);
+					glutSpecialUpFunc(callbacks::special_up);
+					break;
+
+				case event::mouse::click:  signature_assert<void(mouse, int, int)>(function);
+					glutMouseFunc(callbacks::mouse_click);
+					break;
+
+				case event::mouse::motion: signature_assert<void(mouse, int, int)>(function);
+					glutMotionFunc(callbacks::mouse_motion);
+					glutPassiveMotionFunc(callbacks::mouse_motion_passive);
+					break;
+
+				default:
+					throw std::invalid_argument("unknown callback");
+			}
+
+			if (callbacks::closures.find(Callback) != callbacks::closures.end()) {
+				throw std::invalid_argument("the callback already exists");
+			}
+
+			callbacks::closures[Callback] = new decltype(to_function(function))(function);
+		}
+	}
+}
---
+++ source/window.cpp
@@ -0,0 +1,422 @@
+/** Copyleft (ɔ) meh. - http://meh.schizofreni.co
+ *
+ * This file is part of gloglotto - https://github.com/meh/gloglotto
+ *
+ * gloglotto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * gloglotto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with gloglotto. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gloglotto/window>
+
+namespace gloglotto
+{
+	namespace window
+	{
+		void
+		create (int* argc, char* argv[], sized_initializer_list<2, int> size, std::string name) throw (std::invalid_argument)
+		{
+			glutInit(argc, argv);
+			glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+
+			glutInitWindowSize(size[0], size[1]);
+
+			glutCreateWindow(name.c_str());
+		}
+
+		void
+		create (int* argc, char* argv[], sized_initializer_list<2, int> size, sized_initializer_list<2, int> position, std::string name) throw (std::invalid_argument)
+		{
+			glutInit(argc, argv);
+			glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+
+			glutInitWindowSize(size[0], size[1]);
+			glutInitWindowPosition(position[0], position[1]);
+
+			glutCreateWindow(name.c_str());
+		}
+
+		void
+		loop (void)
+		{
+			glutMainLoop();
+		}
+
+		void
+		loop (std::function<void(void)> setup)
+		{
+			setup();
+			loop();
+		}
+
+		namespace callbacks
+		{
+			std::map<int, void*> closures;
+
+			template <typename Signature, typename ...Args>
+			static inline
+			void
+			closure (int type, Args... args) throw (std::invalid_argument)
+			{
+				if (closures.find(type) == closures.end()) {
+					throw std::invalid_argument("unknown callback");
+				}
+
+				(*static_cast<std::function<Signature>*>(closures[type]))(args...);
+			}
+
+			void
+			resize (int width, int height)
+			{
+				closure<void(int, int)>(event::resize, width, height);
+			}
+
+			void
+			render (void)
+			{
+				closure<void(void)>(event::render);
+			}
+
+			void
+			idle (void)
+			{
+				closure<void(void)>(event::idle);
+			}
+
+			void
+			visible (int state)
+			{
+				closure<void(bool)>(event::visible, state == GLUT_VISIBLE);
+			}
+
+			void
+			keyboard (unsigned char ch, int x, int y)
+			{
+				closure<void(key, int, int)>(event::keyboard, key(ch, glutGetModifiers()), x, y);
+			}
+
+			void
+			keyboard_up (unsigned char ch, int x, int y)
+			{
+				closure<void(key, int, int)>(event::keyboard, key(ch, glutGetModifiers(), true), x, y);
+			}
+
+			void
+			special (int ch, int x, int y)
+			{
+				closure<void(key, int, int)>(event::keyboard, key(ch, glutGetModifiers()), x, y);
+			}
+
+			void
+			special_up (int ch, int x, int y)
+			{
+				closure<void(key, int, int)>(event::keyboard, key(ch, glutGetModifiers(), true), x, y);
+			}
+
+			static mouse mouse_current;
+
+			void
+			mouse_click (int button, int state, int x, int y)
+			{
+				if (state == GLUT_DOWN) {
+					mouse_current._button    = mouse::cast(button);
+					mouse_current._modifiers = glutGetModifiers();
+					mouse_current._released  = false;
+
+					closure<void(mouse, int, int)>(event::mouse::click, mouse_current, x, y);
+				}
+				else {
+					mouse_current._released = true;
+
+					closure<void(mouse, int, int)>(event::mouse::click, mouse_current, x, y);
+
+					mouse_current._button    = mouse::cast(0);
+					mouse_current._modifiers = 0;
+				}
+			}
+
+			void
+			mouse_motion (int x, int y)
+			{
+				closure<void(mouse, int, int)>(event::mouse::motion, mouse_current, x, y);
+			}
+
+			void
+			mouse_motion_passive (int x, int y)
+			{
+				closure<void(mouse, int, int)>(event::mouse::motion, mouse(), x, y);
+			}
+		}
+
+		bool
+		key::known (int special)
+		{
+			switch (special) {
+				case F1:
+				case F2:
+				case F3:
+				case F4:
+				case F5:
+				case F6:
+				case F7:
+				case F8:
+				case F9:
+				case F10:
+				case F11:
+				case F12:
+
+				case left:
+				case right:
+				case up:
+				case down:
+
+				case page_up:
+				case page_down:
+				case home:
+				case end:
+				case insert:
+					return true;
+
+				default:
+					return false;
+			}
+		}
+
+		key::key (int modifiers, bool released)
+		{
+			_key       = none << 16;
+			_modifiers = modifiers;
+			_released  = released;
+		}
+
+		key::key (unsigned char key, int modifiers, bool released)
+		{
+			if (key >= 1 && key <= 26 && key != tab && key != enter && key != backspace) {
+				_key = key + 96;
+			}
+			else {
+				_key = std::tolower(key);
+			}
+
+			_modifiers = modifiers;
+			_released  = released;
+		}
+
+		key::key (int key, int modifiers, bool released)
+		{
+			_key       = (known(key) ? key : none) << 16;
+			_modifiers = modifiers;
+			_released  = released;
+		}
+
+		key::key (key const& from)
+		{
+			_key       = from._key;
+			_modifiers = from._modifiers;
+			_released  = from._released;
+		}
+
+		bool
+		key::operator == (unsigned char other)
+		{
+			return _key == other;
+		}
+
+		bool
+		key::operator == (int other)
+		{
+			if (known(other)) {
+				return _key == (other << 16);
+			}
+			else {
+				return _key == (none << 16);
+			}
+		}
+
+		bool
+		key::operator == (key const& other)
+		{
+			return _key == other._key && _modifiers == other._modifiers && _released == other._released;
+		}
+
+		bool
+		key::operator != (unsigned char other)
+		{
+			return _key != std::tolower(other);
+		}
+
+		bool
+		key::operator != (int other)
+		{
+			if (known(other)) {
+				return _key != (other << 16);
+			}
+			else {
+				return _key != (none << 16);
+			}
+		}
+
+		bool
+		key::operator != (key const& other)
+		{
+			return _key != other._key || _modifiers != other._modifiers || _released != other._released;
+		}
+
+		bool
+		key::special (void)
+		{
+			return (_key & 0xff) == 0;
+		}
+
+		unsigned char
+		key::ch (void) throw (std::logic_error)
+		{
+			if (special()) {
+				throw std::logic_error("this is a special key");
+			}
+
+			return _key & 0xff;
+		}
+
+		std::string
+		key::name (void)
+		{
+			if (special()) {
+				switch (_key >> 16) {
+					case F1:  return "F1";
+					case F2:  return "F2";
+					case F3:  return "F3";
+					case F4:  return "F4";
+					case F5:  return "F5";
+					case F6:  return "F6";
+					case F7:  return "F7";
+					case F8:  return "F8";
+					case F9:  return "F9";
+					case F10: return "F10";
+					case F11: return "F11";
+					case F12: return "F12";
+
+					case left:  return "Left";
+					case right: return "Right";
+					case up:    return "Up";
+					case down:  return "Down";
+
+					case page_up:   return "PageUp";
+					case page_down: return "PageDown";
+					case home:      return "Home";
+					case end:       return "End";
+					case insert:    return "Insert";
+
+					case none: return "None";
+				}
+			}
+			else {
+				switch (ch()) {
+					case esc:       return "Esc";
+					case space:     return "Space";
+					case tab:       return "Tab";
+					case enter:     return "Enter";
+					case backspace: return "Backspace";
+					case cancel:    return "Cancel";
+
+					default: return std::string(1, ch());
+				}
+			}
+
+			throw std::logic_error("unreachable");
+		}
+
+		bool
+		key::alt (void)
+		{
+			return _modifiers & GLUT_ACTIVE_ALT;
+		}
+
+		bool
+		key::shift (void)
+		{
+			return _modifiers & GLUT_ACTIVE_SHIFT;
+		}
+
+		bool
+		key::ctrl (void)
+		{
+			return _modifiers & GLUT_ACTIVE_CTRL;
+		}
+
+		bool
+		key::released (void)
+		{
+			return _released;
+		}
+
+		mouse::button
+		mouse::cast (int value)
+		{
+			switch (value) {
+				case GLUT_LEFT_BUTTON:
+					return button::left;
+
+				case GLUT_MIDDLE_BUTTON:
+					return button::middle;
+
+				case GLUT_RIGHT_BUTTON:
+					return button::right;
+			}
+
+			return button::none;
+		}
+
+		mouse::mouse (int modifiers, bool released)
+		{
+			_button    = button::none;
+			_modifiers = modifiers;
+			_released  = released;
+		}
+
+		mouse::mouse (int button, int modifiers, bool released)
+		{
+			_button    = cast(button);
+			_modifiers = modifiers;
+			_released  = released;
+		}
+
+		mouse::mouse (mouse const& from)
+		{
+			*this = from;
+		}
+
+		bool
+		mouse::alt (void)
+		{
+			return _modifiers & GLUT_ACTIVE_ALT;
+		}
+
+		bool
+		mouse::shift (void)
+		{
+			return _modifiers & GLUT_ACTIVE_SHIFT;
+		}
+
+		bool
+		mouse::ctrl (void)
+		{
+			return _modifiers & GLUT_ACTIVE_CTRL;
+		}
+
+		bool
+		mouse::released (void)
+		{
+			return _released;
+		}
+	}
+}
