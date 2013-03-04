9a1232b1877fb7f9671ebd6bb42a285c6f5021aa
meh <meh@schizofreni.co>
2013-03-04 18:22:55 +0100

    example: fix keyboard to new signature

--- examples/keyboard.cpp
+++ examples/keyboard.cpp
@@ -5,7 +5,7 @@ int
 main (int argc, char* argv[])
 {
 	gl::window::create(&argc, argv, { 800, 600 }, "glblblgbllgblglbl");
-	gl::window::on<gl::window::event::keyboard>([](gl::window::key key, int x, int y) {
+	gl::window::on<gl::window::event::keyboard>([](gl::window::key& key, int x, int y) {
 		std::cout << key.name() << " (" << x << ", " << y << ")" << std::endl;
 	});
 
