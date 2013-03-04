aa3f5313bf53e509ce801e286e3ce1a9574e59a2
meh <meh@schizofreni.co>
2013-03-04 18:23:36 +0100

    example: add triangle example

---
+++ examples/triangle.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <gl>
+
+int
+main (int argc, char* argv[])
+{
+	gl::window::create(&argc, argv, { 800, 600 }, "Triangle");
+
+	gl::shader_manager shaders;
+	shaders.add("identity", {
+		{ "vertex", R"(
+			attribute vec3 vertex;
+
+			void
+			main (void)
+			{
+				gl_Position = vec4(vertex.x, vertex.y, vertex.z, 1.0);
+			}
+		)" },
+
+		{ "fragment", R"(
+			uniform vec3 color;
+
+			void
+			main (void)
+			{
+				gl_FragColor = vec4(color.r, color.g, color.b, 1.0);
+			}
+		)" } },
+	[](gl::shader& shader, gl::vector<3> color) {
+		shader.uniform("color", color);
+	});
+
+	gl::window::on<gl::window::event::keyboard>([](gl::window::key& key, int x, int y) {
+		(void) x;
+		(void) y;
+
+		if (key == gl::window::key::esc || key == 'q') {
+			gl::window::exit();
+		}
+	});
+
+	gl::window::on<gl::window::event::resize>([](int w, int h) {
+		gl::viewport(w, h);
+	});
+
+	gl::window::on<gl::window::event::render>([&] {
+		static gl::vectors<3, gl::vector<3>> triangle {
+			{  0.75,  0.75, 0.0 },
+			{  0.75, -0.75, 0.0 },
+			{ -0.75, -0.75, 0.0 } };
+
+		static gl::buffer buffer(gl::target::vertex::data, gl::usage::dynamic::draw, triangle);
+
+		triangle *= gl::make::rotation(gl::angle::radians::make(0.01), 1.0f, 1.0f, 1.0f);
+		buffer.replace(gl::target::vertex::data, triangle);
+
+		gl::clear(gl::clear::color | gl::clear::depth | gl::clear::stencil);
+
+		with_shader(shaders, "identity", gl::vector<3> { 1.0, 1.0, 1.0 }) {
+			self.attribute<gl::vector<3>>("vertex", buffer);
+
+			self.draw(gl::primitive::triangle, 3);
+		}
+
+		gl::window::render();
+		gl::window::update();
+	});
+
+	gl::window::loop([] {
+		gl::clear(gl::clear::color, 0, 0, 0);
+	});
+}
