caf17bc2bdf719c2e519dfe8312c3ee1c4d8c63c
meh <meh@schizofreni.co>
2013-03-18 20:09:19 +0100

    tests: update amirite

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -9,51 +9,51 @@ main (int argc, char* argv[])
 			{
 				gl::matrix<2> m;
 
-				ami::equal(m[0][0], 0);
-				ami::equal(m[0][1], 0);
+				amiequal(m[0][0], 0);
+				amiequal(m[0][1], 0);
 
-				ami::equal(m[1][0], 0);
-				ami::equal(m[1][1], 0);
+				amiequal(m[1][0], 0);
+				amiequal(m[1][1], 0);
 			}
 
 			{
 				gl::matrix<3> m;
 
-				ami::equal(m[0][0], 0);
-				ami::equal(m[0][1], 0);
-				ami::equal(m[0][2], 0);
+				amiequal(m[0][0], 0);
+				amiequal(m[0][1], 0);
+				amiequal(m[0][2], 0);
 
-				ami::equal(m[1][0], 0);
-				ami::equal(m[1][1], 0);
-				ami::equal(m[1][2], 0);
+				amiequal(m[1][0], 0);
+				amiequal(m[1][1], 0);
+				amiequal(m[1][2], 0);
 
-				ami::equal(m[2][0], 0);
-				ami::equal(m[2][1], 0);
-				ami::equal(m[2][2], 0);
+				amiequal(m[2][0], 0);
+				amiequal(m[2][1], 0);
+				amiequal(m[2][2], 0);
 			}
 
 			{
 				gl::matrix<4> m;
 
-				ami::equal(m[0][0], 0);
-				ami::equal(m[0][1], 0);
-				ami::equal(m[0][2], 0);
-				ami::equal(m[0][3], 0);
+				amiequal(m[0][0], 0);
+				amiequal(m[0][1], 0);
+				amiequal(m[0][2], 0);
+				amiequal(m[0][3], 0);
 
-				ami::equal(m[1][0], 0);
-				ami::equal(m[1][1], 0);
-				ami::equal(m[1][2], 0);
-				ami::equal(m[1][3], 0);
+				amiequal(m[1][0], 0);
+				amiequal(m[1][1], 0);
+				amiequal(m[1][2], 0);
+				amiequal(m[1][3], 0);
 
-				ami::equal(m[2][0], 0);
-				ami::equal(m[2][1], 0);
-				ami::equal(m[2][2], 0);
-				ami::equal(m[2][3], 0);
+				amiequal(m[2][0], 0);
+				amiequal(m[2][1], 0);
+				amiequal(m[2][2], 0);
+				amiequal(m[2][3], 0);
 
-				ami::equal(m[3][0], 0);
-				ami::equal(m[3][1], 0);
-				ami::equal(m[3][2], 0);
-				ami::equal(m[3][3], 0);
+				amiequal(m[3][0], 0);
+				amiequal(m[3][1], 0);
+				amiequal(m[3][2], 0);
+				amiequal(m[3][3], 0);
 			}
 		}},
 
@@ -61,51 +61,51 @@ main (int argc, char* argv[])
 			{
 				gl::matrix<2> m(1);
 
-				ami::equal(m[0][0], 1);
-				ami::equal(m[0][1], 0);
+				amiequal(m[0][0], 1);
+				amiequal(m[0][1], 0);
 
-				ami::equal(m[1][0], 0);
-				ami::equal(m[1][1], 1);
+				amiequal(m[1][0], 0);
+				amiequal(m[1][1], 1);
 			}
 
 			{
 				gl::matrix<3> m(1);
 
-				ami::equal(m[0][0], 1);
-				ami::equal(m[0][1], 0);
-				ami::equal(m[0][2], 0);
+				amiequal(m[0][0], 1);
+				amiequal(m[0][1], 0);
+				amiequal(m[0][2], 0);
 
-				ami::equal(m[1][0], 0);
-				ami::equal(m[1][1], 1);
-				ami::equal(m[1][2], 0);
+				amiequal(m[1][0], 0);
+				amiequal(m[1][1], 1);
+				amiequal(m[1][2], 0);
 
-				ami::equal(m[2][0], 0);
-				ami::equal(m[2][1], 0);
-				ami::equal(m[2][2], 1);
+				amiequal(m[2][0], 0);
+				amiequal(m[2][1], 0);
+				amiequal(m[2][2], 1);
 			}
 
 			{
 				gl::matrix<4> m(1);
 
-				ami::equal(m[0][0], 1);
-				ami::equal(m[0][1], 0);
-				ami::equal(m[0][2], 0);
-				ami::equal(m[0][3], 0);
+				amiequal(m[0][0], 1);
+				amiequal(m[0][1], 0);
+				amiequal(m[0][2], 0);
+				amiequal(m[0][3], 0);
 
-				ami::equal(m[1][0], 0);
-				ami::equal(m[1][1], 1);
-				ami::equal(m[1][2], 0);
-				ami::equal(m[1][3], 0);
+				amiequal(m[1][0], 0);
+				amiequal(m[1][1], 1);
+				amiequal(m[1][2], 0);
+				amiequal(m[1][3], 0);
 
-				ami::equal(m[2][0], 0);
-				ami::equal(m[2][1], 0);
-				ami::equal(m[2][2], 1);
-				ami::equal(m[2][3], 0);
+				amiequal(m[2][0], 0);
+				amiequal(m[2][1], 0);
+				amiequal(m[2][2], 1);
+				amiequal(m[2][3], 0);
 
-				ami::equal(m[3][0], 0);
-				ami::equal(m[3][1], 0);
-				ami::equal(m[3][2], 0);
-				ami::equal(m[3][3], 1);
+				amiequal(m[3][0], 0);
+				amiequal(m[3][1], 0);
+				amiequal(m[3][2], 0);
+				amiequal(m[3][3], 1);
 			}
 		}},
 
@@ -114,7 +114,7 @@ main (int argc, char* argv[])
 			gl::matrix<2, 2> b = {{3, 4}, {4, 3}};
 			gl::matrix<2, 2> c = a + b;
 
-			ami::truthy(c[0][0] == 4 && c[0][1] == 6 &&
+			amitruthy(c[0][0] == 4 && c[0][1] == 6 &&
 			            c[1][0] == 6 && c[1][1] == 4);
 		}},
 
@@ -123,7 +123,7 @@ main (int argc, char* argv[])
 			gl::matrix<2, 2> b = {{3, 4}, {4, 3}};
 			gl::matrix<2, 2> c = a - b;
 
-			ami::truthy(c[0][0] == -2 && c[0][1] == -2 &&
+			amitruthy(c[0][0] == -2 && c[0][1] == -2 &&
 			            c[1][0] == -2 && c[1][1] == -2);
 		}},
 
@@ -131,11 +131,11 @@ main (int argc, char* argv[])
 			gl::matrix<2, 2> a = {{1, 2}, {2, 1}};
 			gl::matrix<2, 2> b = a * 2;
 
-			ami::equal(b[0][0], 2);
-			ami::equal(b[0][1], 4);
+			amiequal(b[0][0], 2);
+			amiequal(b[0][1], 4);
 
-			ami::equal(b[1][0], 4);
-			ami::equal(b[1][1], 2);
+			amiequal(b[1][0], 4);
+			amiequal(b[1][1], 2);
 		}},
 
 		{ "* (vector)", []{
@@ -143,8 +143,8 @@ main (int argc, char* argv[])
 			gl::vector<2>    b = {5, 5};
 			gl::vector<2>    c = a * b;
 
-			ami::equal(c[0], 15);
-			ami::equal(c[1], 15);
+			amiequal(c[0], 15);
+			amiequal(c[1], 15);
 		}},
 
 		{ "* (matrix)", []{
@@ -152,17 +152,17 @@ main (int argc, char* argv[])
 			gl::matrix<3, 3> b = {{4,5,6},{6,5,4},{4,6,5}};
 			gl::matrix<3, 3> c = a * b;
 
-			ami::equal(c[0][0], 28);
-			ami::equal(c[0][1], 33);
-			ami::equal(c[0][2], 29);
+			amiequal(c[0][0], 28);
+			amiequal(c[0][1], 33);
+			amiequal(c[0][2], 29);
 
-			ami::equal(c[1][0], 28);
-			ami::equal(c[1][1], 31);
-			ami::equal(c[1][2], 31);
+			amiequal(c[1][0], 28);
+			amiequal(c[1][1], 31);
+			amiequal(c[1][2], 31);
 
-			ami::equal(c[2][0], 26);
-			ami::equal(c[2][1], 33);
-			ami::equal(c[2][2], 31);
+			amiequal(c[2][0], 26);
+			amiequal(c[2][1], 33);
+			amiequal(c[2][2], 31);
 		}}
 	});
 }
