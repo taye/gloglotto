1751ec4d71cfd69d7ceea3833e1c2e4840181220
meh <meh@schizofreni.co>
2013-03-24 02:24:32 +0100

    tests/rgba: test for iterator

--- tests/rgba.cpp
+++ tests/rgba.cpp
@@ -23,6 +23,35 @@ main (int argc, char* argv[])
 			amiequal(colors[0]->g, 2);
 			amiequal(colors[0]->b, 2);
 			amiequal(colors[0]->a, 0);
+		}},
+
+		{ "foreach", []{
+			gl::image::format::rgba<2> colors(2);
+
+			colors[0]->r = 1;
+			colors[0]->g = 2;
+			colors[0]->b = 2;
+
+			int i = 0;
+			for (auto color : colors) {
+				switch (i) {
+					case 0:
+						amiequal(color->r, 1);
+						amiequal(color->g, 2);
+						amiequal(color->b, 2);
+						amiequal(color->a, 0);
+						break;
+
+					case 1:
+						amiequal(color->r, 0);
+						amiequal(color->g, 0);
+						amiequal(color->b, 0);
+						amiequal(color->a, 0);
+						break;
+				}
+
+				i++;
+			}
 		}}
 	});
 }
